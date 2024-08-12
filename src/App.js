df23a462cf5bb6f8f0168b73451e335a3a91c7b1
Bikash <medhi01011997@gmail.com>
2024-08-12 07:34:15 +0530

    Deleting a Blog

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [content, setContent] = useState("");
+  const [formData, setFormData] = useState({ title: "", content: "" });
   const [blogs, setBlogs] = useState([]);
 
   function submitHandler(e) {
     e.preventDefault();
-    setBlogs([{ title, content }, ...blogs]);
+    setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+
+    setFormData({ title: "", content: "" });
   }
+
+  const removeBlog = (index) => {
+    const newBlogs = blogs.filter((blog, i) => i !== index);
+    setBlogs(newBlogs);
+    console.log("remove");
+    console.log(index);
+  };
 
   return (
     <div className="App">
@@ -20,8 +30,10 @@ function App() {
             name=""
             id=""
             placeholder="Enter your title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={formData.title}
+            onChange={(e) =>
+              setFormData({ title: e.target.value, content: formData.content })
+            }
           />
           <textarea
             type="text"
@@ -29,10 +41,12 @@ function App() {
             id=""
             className="textarea"
             placeholder="Enter your content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={formData.content}
+            onChange={(e) =>
+              setFormData({ title: formData.title, content: e.target.value })
+            }
           />
-          <button className="btn">Add</button>
+          <button className="addbtn">Add</button>
         </form>
       </div>
 
@@ -42,6 +56,9 @@ function App() {
           <div className="blog" key={index}>
             <h2>{blog.title}</h2>
             <p>{blog.content}</p>
+            <button className="removeBtn" onClick={() => removeBlog(index)}>
+              Remove
+            </button>
           </div>
         );
       })}
