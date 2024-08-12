4f410c6826202ad32955c34aa1aa64b4b5113740
Bikash <medhi01011997@gmail.com>
2024-08-12 07:52:09 +0530

    Change the document.title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
   // const [title, setTitle] = useState("");
   // const [content, setContent] = useState("");
   const [formData, setFormData] = useState({ title: "", content: "" });
   const [blogs, setBlogs] = useState([]);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    titleRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    if (blogs.length && blogs[0].title) {
+      document.title = blogs[0].title;
+    } else {
+      document.title = "No Blogs";
+    }
+  }, [blogs]);
 
   function submitHandler(e) {
     e.preventDefault();
     setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
 
     setFormData({ title: "", content: "" });
+    titleRef.current.focus();
   }
 
   const removeBlog = (index) => {
@@ -34,6 +48,7 @@ function App() {
             onChange={(e) =>
               setFormData({ title: e.target.value, content: formData.content })
             }
+            ref={titleRef}
           />
           <textarea
             type="text"
@@ -45,6 +60,7 @@ function App() {
             onChange={(e) =>
               setFormData({ title: formData.title, content: e.target.value })
             }
+            required
           />
           <button className="addbtn">Add</button>
         </form>
